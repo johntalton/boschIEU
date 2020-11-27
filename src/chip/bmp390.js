7d9e017c65835792a03a204e86156746233ea835
johntalton <johntalton@gmail.com>
2020-11-27 15:59:26 -0500

    remaining common into 3xx

--- src/chip/bmp390.js
+++ src/chip/bmp390.js
@@ -1,5 +1,3 @@
-const { BusUtil } = require('@johntalton/and-other-delights');
-
 const { bmp3xx } = require('./bmp3xx.js');
 
 /**
@@ -8,16 +6,6 @@ const { bmp3xx } = require('./bmp3xx.js');
 class bmp390 extends bmp3xx {
   static get name() { return 'bmp390'; }
   static get chipId() { return 0x60; }
-
-  static sensorTime(bus) {
-    return BusUtil.readBlock(bus, [[0xC0, 3]]).then(buffer => { throw new Error('read24'); });
-  }
-
-  static profile(bus) {
-    return BusUtil.readBlock(bus, [[0x15, 11]]).then(buffer => {
-      return {};
-    });
-  }
 }
 
 module.exports = { bmp390 };
